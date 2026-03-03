1fe74c0fd54b509bebe27008d8b74b6a72d8db51
naeemulhaq6477-dotcom <naeemulhaq6477@gmail.com>
2026-03-03 12:38:03 -0800

    lab 2 files

---
+++ lab 2/lab 2 tasks/task 2 floating point variable.cpp
@@ -0,0 +1,12 @@
+// Name: Naeem Ul Haq
+// Roll No: 5122416 / 0665
+// Lab 02 - Task 2
+
+#include <iostream>   // Include input-output stream library
+using namespace std;  // Use standard namespace
+
+int main() {
+    float pi = 3.14;   // Declare floating-point variable pi and assign value 3.14
+    cout << "The value of pi is: " << pi << endl;  // Print the value of pi
+    return 0;  // End program
+}
---
+++ lab 2/lab 2 tasks/task 3 celcius to farenhite.cpp
@@ -0,0 +1,17 @@
+// Name: Naeem Ul Haq
+// Roll No: 5122416 / 0665
+// Lab 02 - Task 3
+
+#include <iostream>   // Include input-output stream library
+using namespace std;  // Use standard namespace
+
+int main() {
+    double celsius;   // Declare variable to store Celsius temperature
+    cout << "Enter temperature in Celsius: ";  // Prompt user for input
+    cin >> celsius;   // Read Celsius value from user
+
+    double fahrenheit = (celsius * 9.0 / 5.0) + 32; // Convert Celsius to Fahrenheit using formula
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl; // Print result
+
+    return 0;  // End program
+}
