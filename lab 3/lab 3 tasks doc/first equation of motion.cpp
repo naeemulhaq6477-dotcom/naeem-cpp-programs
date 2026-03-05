1b4aaa2b0dfef02c35fa9612e25eae6dd1c46571
naeemulhaq6477-dotcom <naeemulhaq6477@gmail.com>
2026-03-05 09:59:09 -0800

    Update first equation of motion.cpp

--- lab 3/lab 3 tasks doc/first equation of motion.cpp
+++ lab 3/lab 3 tasks doc/first equation of motion.cpp
@@ -1,25 +1,25 @@
                           // NAME = NAEEM UL HAQ
                            // ID = 5122416 / 0665
 
-#include <iostream>
-using namespace std;
+#include <iostream>          // this tells the compiler to include standard input output library
+using namespace std;   // this allows us to use standard namespace instead of writing std:: for each cin and cout
 
 int main() {                    // START OF MAIN FUNCTION
 	
 	double v,u,a,t;             // variable declaration
 	
 	cout<<"enter u ";            // prompt for initial velocity
-	cin>>u;
+	cin>>u;                   // takes input value and stores it in variable
 	cout<<"enter a ";            // prompt for acceleration
-	cin>>a;
+	cin>>a;                     // takes input value and stores it in variable
 	cout<<"enter t ";    // prompt for time
 	cin>>t;
 	
 	v=u+a*t;               // calculate final velocity using equation
 	
-	cout<<"initial velocity = "<<u<<" m/s"<<endl;   // initial velocity
-	cout<<"acceleration = "<<a<<"m/s^2"<<endl;      // acceleration
-	cout<<"time = "<<t<<" s" <<endl;               // time
-	cout<<"final velocity = "<<v<<" m/s"<<endl;    // final velocity
+	cout<<"initial velocity = "<<u<<" m/s"<<endl;   // initial velocity and unit displaying 
+	cout<<"acceleration = "<<a<<"m/s^2"<<endl;      // acceleration and its unit  displayng
+	cout<<"time = "<<t<<" s" <<endl;               // time and unit displaying
+	cout<<"final velocity = "<<v<<" m/s"<<endl;    // final velocity  and its unit displaying
 
 }
